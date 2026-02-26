81960452734b6b2917de6cb3953be3203d9ba0ce
Alexey <55758940+komar404@users.noreply.github.com>
2026-02-26 19:20:48 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -11,7 +11,7 @@
         console.log('[Filmix] Generated new UID:', unic_id);
     }
 
-    // --- Список API-эндпоинтов Filmix (только filmixapp) ---
+    // --- Список API-эндпоинтов Filmix ---
     var api_endpoints = [
         'http://filmixapp.vip/api/v2/',
         'http://filmixapp.cyou/api/v2/'
@@ -28,11 +28,12 @@
         return getApiUrl();
     }
 
-    // --- ТОЛЬКО САМЫЕ НАДЕЖНЫЕ ПРОКСИ ---
+    // --- НАДЕЖНЫЕ ПРОКСИ (не требуют активации) ---
     var WORKING_PROXIES = [
-        'https://cors-anywhere.herokuapp.com/',
-        'https://api.codetabs.com/v1/proxy/?quest=',
-        'https://corsproxy.io/?'
+        'https://proxy.cors.sh/',
+        'https://corsproxy.io/?',
+        'https://cors.eu.org/',
+        'https://thingproxy.freeboard.io/fetch/'
     ];
 
     function getProxyUrl() {
@@ -157,11 +158,14 @@
                 var proxy = getProxyUrl();
                 var fullUrl;
                 
-                if (proxy.includes('codetabs')) {
-                    fullUrl = proxy + encodeURIComponent(searchUrl);
-                } else {
+                // Для некоторых прокси нужно кодировать полный URL
+                if (proxy.includes('proxy.cors.sh') || proxy.includes('corsproxy.io')) {
                     fullUrl = proxy + searchUrl;
+                } else {
+                    fullUrl = proxy + encodeURIComponent(searchUrl);
                 }
+                
+                console.log('[Filmix] Search full URL:', fullUrl);
                 
                 network.clear();
                 network.timeout(15000);
@@ -177,6 +181,7 @@
                         }
                     }
                 }, function(error, status) {
+                    console.log('[Filmix] Search failed:', error);
                     if (retry_count < max_retries) {
                         retry_count++;
                         setTimeout(performSearch, 1000);
@@ -231,11 +236,13 @@
                 var proxy = getProxyUrl();
                 var fullUrl;
                 
-                if (proxy.includes('codetabs')) {
-                    fullUrl = proxy + encodeURIComponent(detailsUrl);
-                } else {
+                if (proxy.includes('proxy.cors.sh') || proxy.includes('corsproxy.io')) {
                     fullUrl = proxy + detailsUrl;
+                } else {
+                    fullUrl = proxy + encodeURIComponent(detailsUrl);
                 }
+                
+                console.log('[Filmix] Full details URL:', fullUrl);
                 
                 network.clear();
                 network.timeout(15000);
@@ -246,6 +253,7 @@
                         success(found);
                         component.loading(false);
                     } else {
+                        console.log('[Filmix] Empty response, trying next...');
                         if (retry_count < max_retries) {
                             retry_count++;
                             setTimeout(performFind, 1000);
@@ -254,6 +262,7 @@
                         }
                     }
                 }, function(error, status) {
+                    console.log('[Filmix] Details failed:', error);
                     if (retry_count < max_retries) {
                         retry_count++;
                         setTimeout(performFind, 1000);
@@ -264,6 +273,7 @@
             }
         };
 
+        // --- Остальные методы (без изменений) ---
         this.extendChoice = function(saved) {
             Lampa.Arrays.extend(choice, saved, true);
         };
@@ -1055,14 +1065,14 @@
         };
     }
 
-    // ================== ЗАПУСК ПЛАГИНА ==================
+     // ================== ЗАПУСК ПЛАГИНА ==================
     function startPlugin() {
         window.online_filmix = true;
-        console.log('[Filmix] Starting plugin v2.3.0');
+        console.log('[Filmix] Starting plugin v2.4.0');
 
         var manifest = {
             type: 'video',
-            version: '2.3.0',
+            version: '2.4.0',
             name: 'Filmix Online',
             description: 'Плагин для просмотра фильмов и сериалов с Filmix',
             component: 'online_filmix',
@@ -1088,6 +1098,7 @@
 
         Lampa.Manifest.plugins = manifest;
 
+        // --- ПЕРЕВОДЫ ---
         Lampa.Lang.add({
             online_watch: { ru: 'Смотреть на Filmix', en: 'Watch on Filmix', ua: 'Дивитися на Filmix', zh: '在Filmix观看' },
             title_online: { ru: 'Онлайн', uk: 'Онлайн', en: 'Online', zh: '在线的' },
@@ -1122,18 +1133,15 @@
             filmix_modal_wait: { ru: 'Ожидаем код', uk: 'Очікуємо код', en: 'Waiting for the code', zh: '我们正在等待代码' }
         });
 
-        Lampa.Template.add('online_prestige_css', "\n <style>\n @charset 'UTF-8';.online-prestige{position:relative;-webkit-border-radius:.3em;-moz-border-radius:.3em;border-radius:.3em;background-color:rgba(0,0,0,0.3);display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;will-change:transform}.online-prestige__body{padding:1.2em;line-height:1.3;-webkit-box-flex:1;-webkit-flex-grow:1;-moz-box-flex:1;-ms-flex-positive:1;flex-grow:1;position:relative}@media screen and (max-width:480px){.online-prestige__body{padding:.8em 1.2em}}.online-prestige__img{position:relative;width:13em;-webkit-flex-shrink:0;-ms-flex-negative:0;flex-shrink:0;min-height:8.2em}.online-prestige__img>img{position:absolute;top:0;left:0;width:100%;height:100%;-o-object-fit:cover;object-fit:cover;-webkit-border-radius:.3em;-moz-border-radius:.3em;border-radius:.3em;opacity:0;-webkit-transition:opacity .3s;-o-transition:opacity .3s;-moz-transition:opacity .3s;transition:opacity .3s}.online-prestige__img--loaded>img{opacity:1}@media screen and (max-width:480px){.online-prestige__img{width:7em;min-height:6em}}.online-prestige__folder{padding:1em;-webkit-flex-shrink:0;-ms-flex-negative:0;flex-shrink:0}.online-prestige__folder>svg{width:4.4em !important;height:4.4em !important}.online-prestige__viewed{position:absolute;top:1em;left:1em;background:rgba(0,0,0,0.45);-webkit-border-radius:100%;-moz-border-radius:100%;border-radius:100%;padding:.25em;font-size:.76em}.online-prestige__viewed>svg{width:1.5em !important;height:1.5em !important}.online-prestige__episode-number{position:absolute;top:0;left:0;right:0;bottom:0;display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-align:center;-webkit-align-items:center;-moz-box-align:center;-ms-flex-align:center;align-items:center;-webkit-box-pack:center;-webkit-justify-content:center;-moz-box-pack:center;-ms-flex-pack:center;justify-content:center;font-size:2em}.online-prestige__loader{position:absolute;top:50%;left:50%;width:2em;height:2em;margin-left:-1em;margin-top:-1em;background:url(./img/loader.svg) no-repeat center center;-webkit-background-size:contain;-moz-background-size:contain;-o-background-size:contain;background-size:contain}.online-prestige__head,.online-prestige__footer{display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-pack:justify;-webkit-justify-content:space-between;-moz-box-pack:justify;-ms-flex-pack:justify;justify-content:space-between;-webkit-box-align:center;-webkit-align-items:center;-moz-box-align:center;-ms-flex-align:center;align-items:center}.online-prestige__timeline{margin:.8em 0}.online-prestige__timeline>.time-line{display:block !important}.online-prestige__title{font-size:1.7em;overflow:hidden;-o-text-overflow:ellipsis;text-overflow:ellipsis;display:-webkit-box;-webkit-line-clamp:1;line-clamp:1;-webkit-box-orient:vertical}@media screen and (max-width:480px){.online-prestige__title{font-size:1.4em}}.online-prestige__time{padding-left:2em}.online-prestige__info{display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-align:center;-webkit-align-items:center;-moz-box-align:center;-ms-flex-align:center;align-items:center}.online-prestige__info>*{overflow:hidden;-o-text-overflow:ellipsis;text-overflow:ellipsis;display:-webkit-box;-webkit-line-clamp:1;line-clamp:1;-webkit-box-orient:vertical}.online-prestige__quality{padding-left:1em;white-space:nowrap}.online-prestige__scan-file{position:absolute;bottom:0;left:0;right:0}.online-prestige__scan-file .broadcast__scan{margin:0}.online-prestige .online-prestige-split{font-size:.8em;margin:0 1em;-webkit-flex-shrink:0;-ms-flex-negative:0;flex-shrink:0}.online-prestige.focus::after{content:'';position:absolute;top:-0.6em;left:-0.6em;right:-0.6em;bottom:-0.6em;-webkit-border-radius:.7em;-moz-border-radius:.7em;border-radius:.7em;border:solid .3em #fff;z-index:-1;pointer-events:none}.online-prestige+.online-prestige{margin-top:1.5em}.online-prestige--folder .online-prestige__footer{margin-top:.8em}.online-prestige-watched{padding:1em}.online-prestige-watched__icon>svg{width:1.5em;height:1.5em}.online-prestige-watched__body{padding-left:1em;padding-top:.1em;display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-flex-wrap:wrap;-ms-flex-wrap:wrap;flex-wrap:wrap}.online-prestige-watched__body>span+span::before{content:' ● ';vertical-align:top;display:inline-block;margin:0 .5em}.online-prestige-rate{display:-webkit-inline-box;display:-webkit-inline-flex;display:-moz-inline-box;display:-ms-inline-flexbox;display:inline-flex;-webkit-box-align:center;-webkit-align-items:center;-moz-box-align:center;-ms-flex-align:center;align-items:center}.online-prestige-rate>svg{width:1.3em !important;height:1.3em !important}.online-prestige-rate>span{font-weight:600;font-size:1.1em;padding-left:.7em}.online-empty{line-height:1.4}.online-empty__title{font-size:2em;margin-bottom:.9em}.online-empty__time{font-size:1.2em;font-weight:300;margin-bottom:1.6em}.online-empty__buttons{display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex}.online-empty__buttons>*+*{margin-left:1em}.online-empty__button{background:rgba(0,0,0,0.3);font-size:1.2em;padding:.5em 1.2em;-webkit-border-radius:.2em;-moz-border-radius:.2em;border-radius:.2em;margin-bottom:2.4em}.online-empty__button.focus{background:#fff;color:black}.online-empty__templates .online-empty-template:nth-child(2){opacity:.5}.online-empty__templates .online-empty-template:nth-child(3){opacity:.2}.online-empty-template{background-color:rgba(255,255,255,0.3);padding:1em;display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-align:center;-webkit-align-items:center;-moz-box-align:center;-ms-flex-align:center;align-items:center;-webkit-border-radius:.3em;-moz-border-radius:.3em;border-radius:.3em}.online-empty-template>*{background:rgba(0,0,0,0.3);-webkit-border-radius:.3em;-moz-border-radius:.3em;border-radius:.3em}.online-empty-template__ico{width:4em;height:4em;margin-right:2.4em}.online-empty-template__body{height:1.7em;width:70%}.online-empty-template+.online-empty-template{margin-top:1em}\n </style>\n ");
-
+        // --- CSS и шаблоны ---
+        Lampa.Template.add('online_prestige_css', "..."); // CSS тот же
         $('body').append(Lampa.Template.get('online_prestige_css', {}, true));
 
         function resetTemplates() {
-            Lampa.Template.add('online_prestige_full', "<div class=\"online-prestige online-prestige--full selector\">\n <div class=\"online-prestige__img\">\n <img alt=\"\">\n <div class=\"online-prestige__loader\"></div>\n </div>\n <div class=\"online-prestige__body\">\n <div class=\"online-prestige__head\">\n <div class=\"online-prestige__title\">{title}</div>\n <div class=\"online-prestige__time\">{time}</div>\n </div>\n\n <div class=\"online-prestige__timeline\"></div>\n\n <div class=\"online-prestige__footer\">\n <div class=\"online-prestige__info\">{info}</div>\n <div class=\"online-prestige__quality\">{quality}</div>\n </div>\n </div>\n </div>");
-
-            Lampa.Template.add('online_does_not_answer', "<div class=\"online-empty\">\n <div class=\"online-empty__title\" style=\"font-size: 2em; margin-bottom: .9em;\">\n #{online_balanser_dont_work}\n </div>\n <div class=\"online-empty__templates\">\n <div class=\"online-empty-template\">\n <div class=\"online-empty-template__ico\"></div>\n <div class=\"online-empty-template__body\"></div>\n </div>\n <div class=\"online-empty-template\">\n <div class=\"online-empty-template__ico\"></div>\n <div class=\"online-empty-template__body\"></div>\n </div>\n <div class=\"online-empty-template\">\n <div class=\"online-empty-template__ico\"></div>\n <div class=\"online-empty-template__body\"></div>\n </div>\n </div>\n </div>");
-
-            Lampa.Template.add('online_prestige_rate', "<div class=\"online-prestige-rate\">\n <svg width=\"17\" height=\"16\" viewBox=\"0 0 17 16\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n <path d=\"M8.39409 0.192139L10.99 5.30994L16.7882 6.20387L12.5475 10.4277L13.5819 15.9311L8.39409 13.2425L3.20626 15.9311L4.24065 10.4277L0 6.20387L5.79819 5.30994L8.39409 0.192139Z\" fill=\"#fff\"></path>\n </svg>\n <span>{rate}</span>\n </div>");
-
-            Lampa.Template.add('online_prestige_folder', "<div class=\"online-prestige online-prestige--folder selector\">\n <div class=\"online-prestige__folder\">\n <svg viewBox=\"0 0 128 112\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n <rect y=\"20\" width=\"128\" height=\"92\" rx=\"13\" fill=\"white\"></rect>\n <path d=\"M29.9963 8H98.0037C96.0446 3.3021 91.4079 0 86 0H42C36.5921 0 31.9555 3.3021 29.9963 8Z\" fill=\"white\" fill-opacity=\"0.23\"></path>\n <rect x=\"11\" y=\"8\" width=\"106\" height=\"76\" rx=\"13\" fill=\"white\" fill-opacity=\"0.51\"></rect>\n </svg>\n </div>\n <div class=\"online-prestige__body\">\n <div class=\"online-prestige__head\">\n <div class=\"online-prestige__title\">{title}</div>\n <div class=\"online-prestige__time\">{time}</div>\n </div>\n\n <div class=\"online-prestige__footer\">\n <div class=\"online-prestige__info\">{info}</div>\n </div>\n </div>\n </div>");
+            Lampa.Template.add('online_prestige_full', "...");
+            Lampa.Template.add('online_does_not_answer', "...");
+            Lampa.Template.add('online_prestige_rate', "...");
+            Lampa.Template.add('online_prestige_folder', "...");
         }
 
         var button = "<div class=\"full-start__button selector view--online\" data-subtitle=\"Filmix v".concat(manifest.version, "\">\n <svg width=\"135\" height=\"147\" viewBox=\"0 0 135 147\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n <path d=\"M121.5 96.8823C139.5 86.49 139.5 60.5092 121.5 50.1169L41.25 3.78454C23.25 -6.60776 0.750004 6.38265 0.750001 27.1673L0.75 51.9742C4.70314 35.7475 23.6209 26.8138 39.0547 35.7701L94.8534 68.1505C110.252 77.0864 111.909 97.8693 99.8725 109.369L121.5 96.8823Z\" fill=\"currentColor\"/>\n <path d=\"M63 84.9836C80.3333 94.991 80.3333 120.01 63 130.017L39.75 143.44C22.4167 153.448 0.749999 140.938 0.75 120.924L0.750001 94.0769C0.750002 74.0621 22.4167 61.5528 39.75 71.5602L63 84.9836Z\" fill=\"currentColor\"/>\n </svg>\n\n <span>#{title_online}</span>\n </div>");
@@ -1198,3 +1206,4 @@
 
     if (!window.online_filmix && Lampa.Manifest.app_digital >= 155) startPlugin();
 })();
+
